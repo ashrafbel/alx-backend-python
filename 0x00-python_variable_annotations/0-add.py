3ddb7abe128ead1a369d731a24b861a6767613f9
ashrafbel <ashrafmus2@gmail.com>
2024-10-10 14:59:07 -0700

    addls

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 'add - annotations'
 
-def add(a: float, b:float) -> float:
+
+def add(a: float, b: float) -> float:
     "this functoins returns sum float"
     return a + b
